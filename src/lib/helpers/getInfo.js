2ed050876194fb507512d1ea3dad0251d4705fd0
André do Nascimento Ferreira <andredonascimentoferreira309@gmail.com>
2024-11-25 16:25:28 -0300

    primeiro commit front end 0.0.1

---
+++ src/lib/helpers/getInfo.js
@@ -0,0 +1,85 @@
+import Cookies from "js-cookie";
+import { goto } from "$app/navigation";
+import { error } from "@sveltejs/kit";
+
+
+class GetInfo {
+
+    constructor() {
+        this.info = null
+    }
+
+    connect(email, cpf, senha) {
+        fetch("http://localhost:3000/auth", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email: email, cpf: cpf, senha: senha }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                const { token, refreshToken } = data;
+                Cookies.set("token", `${token}`, { expires: 1, path: "/" });
+                Cookies.set("refreshToken", `${refreshToken}`, {
+                    expires: 1,
+                    path: "/",
+                });
+                Cookies.set("cpf", `${cpf}`, {
+                    path: "/",
+                });
+
+                try {
+                    this.#Get(cpf).then((info) => {
+                        if (info) {
+                            goto("login");
+                        }
+                    }).catch((error) => {
+                        alert('Email/Cpf ou senha errados')
+                        console.error(error)
+                    })
+                } catch (error) {
+
+                }
+            })
+
+            .catch((error) => {
+                console.error("Erro na requisição:", error);
+                alert("Erro ao conectar ao servidor.");
+            });
+    }
+
+    async  #Get(cpf) {
+        await fetch(`http://localhost:3000/?cpf=${cpf}`).then(response => {
+            return response.json();
+        }).then(response => {
+            if (response) {
+                
+                const { nome, departamento, email, cpf, erro } = response
+
+                if (erro) {
+                    throw new Error('usuario não encotrado')
+                }
+
+                return this.info = { nome: nome, departamento: departamento, email: email, cpf: cpf };
+            } else {
+                alert('usuario não encontrado')
+            }
+        })
+        return this.info
+    }
+
+    async Show() {
+
+
+        const cpf = Cookies.get('cpf')
+
+        await this.#Get(cpf).then((data) => {
+            return this.info = data
+        })
+
+        return this.info
+
+    }
+
+}
+
+export default GetInfo
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="js">
+    import GetInfo from "$lib/helpers/getInfo.js";
+    const getinfo = new GetInfo();
+
+    // TODO: fazer aparecer pelomenos um valor retornado do Json do Show();
+    let info = {};
+    let Primeiro_nome = "";
+    getinfo
+        .Show()
+        .then((x) => {
+            info = x;
+            return info;
+        })
+        .then((x) => {
+            Primeiro_nome = x.nome.split(" ")[0];
+            return Primeiro_nome;
+        });
+</script>
+
+<main>
+    <aside>
+        <h3>
+            seja bem vindo {Primeiro_nome}
+        </h3>
+    </aside>
+</main>
+
+<style>
+    main {
+        background-color: tomato;
+        height: 100vh;
+    }
+    aside {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: white;
+        height: 100vh;
+        width: 260px;
+        position: absolute;
+        left: 0px;
+    }
+</style>
