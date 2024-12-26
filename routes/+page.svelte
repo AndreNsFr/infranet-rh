4726864357b0ed28174dbdafb3943ffb9ba84a54
André do Nascimento Ferreira <72326296+AndreNsFr@users.noreply.github.com>
2024-12-25 21:30:17 -0300

    bug fix
    bug fix

---
+++ routes/+page.svelte
@@ -0,0 +1,119 @@
+<script>
+    import GetInfo from "$lib/helpers/getInfo.js";
+    import Cookie from "js-cookie";
+    import { goto } from "$app/navigation";
+
+    const getInfo = new GetInfo();
+
+    function contato(event) {
+        event.preventDefault();
+        const email = document.getElementById("email").value;
+        const cpf = document.getElementById("cpf").value;
+        const senha = document.getElementById("senha").value;
+
+        getInfo.Login(email, cpf, senha);
+    }
+</script>
+
+<main>
+    <section>
+        <form on:submit={contato} method="post">
+            <h1
+                style="text-align: center; margin-top: 10px;
+            margin-bottom:40px; font-size:30pt;"
+            >
+                Login
+            </h1>
+            <label for="email">Digite o seu Email:</label>
+            <input
+                required
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Email"
+            />
+            <label for="cpf">Digite o seu cpf:</label>
+            <input required type="text" name="cpf" id="cpf" placeholder="CPF" />
+            <label required for="senha">Digite o sua senha:</label>
+            <input
+                type="password"
+                name="senha"
+                id="senha"
+                placeholder="Senha"
+                required
+            />
+            <input type="submit" value="Enviar" />
+        </form>
+    </section>
+</main>
+
+<style>
+    :global(body) {
+        font-family: Arial, Helvetica, sans-serif;
+        width: 100vw;
+        margin: 0;
+        padding: 0;
+        
+        overflow-x: hidden;
+    }
+
+    main {
+        display: grid;
+        place-items: center;
+        background-color: tomato;
+        width: 100%;
+        min-height: 100vh;
+        background: url(/background-login.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+    section {
+        backdrop-filter: blur(3px);
+        background-color: rgba(255, 255, 255, 0.767);
+        padding: 15px;
+        border-radius: 15px;
+        height: 600px;
+        box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.705);
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        width: 500px;
+        gap: 15px;
+    }
+
+    form > input {
+        height: 50px;
+        border-radius: 90px;
+        outline: none;
+        border: 2px solid rgba(0, 0, 0, 0.363);
+        text-indent: 1em;
+        font-size: 12pt;
+    }
+    form > input[type="submit"] {
+        font-size: 12pt;
+        margin-top: 110px;
+    }
+
+    form > input[type="text"]:focus,
+    form > input[type="password"]:focus {
+        transition: 0.2s ease-out;
+        border: 2px solid rgb(0, 0, 0);
+    }
+
+    form > input[type="text"]:valid,
+    form > input[type="password"]:valid {
+        border: 2px solid rgb(0, 0, 0);
+    }
+
+    form > input[type="submit"]:active {
+        transition: 0.1s;
+        background-color: rgba(0, 0, 0, 0.507);
+    }
+    form > label {
+        font-size: 13pt;
+        text-indent: 8px;
+    }
+</style>
---
+++ routes/login/ProfileConfigs.svelte
@@ -0,0 +1,192 @@
+<script>
+    import GetInfo from "$lib/helpers/getInfo.js";
+
+    const getInfo = new GetInfo();
+
+    let configs = {};
+
+    getInfo.Show().then((data) => {
+        configs = data;
+        return configs;
+    });
+
+    function sendNewData(event) {
+        event.preventDefault();
+
+        const cpf = configs.cpf;
+
+        let nome = document.getElementById("nome").value;
+        let email = document.getElementById("email").value;
+        let senha = document.getElementById("senha").value;
+        let imagem = document.getElementById("imagem").files[0];
+
+        getInfo
+            .VerifyActions()
+            .then((Response) => {
+                if (Response) {
+                    getInfo.UpdateInfo(cpf, nome, email, imagem, senha).then();
+                }
+            })
+            .catch((erro) => {
+                console.error(erro);
+            });
+    }
+
+    let imagem_status = false;
+    let imagem_selecionada = null;
+
+    async function imagemSelecionada(event) {
+        event.preventDefault();
+
+        let imagem = document.getElementById("imagem").files[0];
+
+        getInfo.toBase64(imagem).then((Imagem) => {
+            return (imagem_selecionada = Imagem);
+        });
+        imagem_status = true;
+        return imagem_selecionada;
+    }
+</script>
+
+<div class="configurações">
+    <div class="seus-dados">
+        <img
+            src={configs.imagem}
+            width="200px"
+            height="200px"
+            alt="imagem-pessoal"
+            style="border-radius: 900px;"
+        />
+        <h3>Nome:</h3>
+        <span>{configs.nome}</span>
+        <h3>Departamento:</h3>
+        <span>{configs.departamento}</span>
+        <h3>Data de Nascimento:</h3>
+        <span>{configs.data}</span>
+        <h3>Email:</h3>
+        <span>{configs.email}</span>
+        <h3>Cpf:</h3>
+        <span>{configs.cpf}</span>
+    </div>
+
+    <form on:submit={sendNewData(event)}>
+        <h1>Alterar suas informações:</h1>
+        <br />
+        <br />
+        <label for="nome">Alterar nome:</label>
+        <input
+            type="text"
+            name="nome"
+            id="nome"
+            placeholder="nome"
+            value={configs.nome}
+        />
+        <label for="senha">Alterar senha:</label>
+        <input type="password" name="senha" id="senha" placeholder="senha" />
+        <label for="email">Alterar email:</label>
+        <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+            value={configs.email}
+        />
+        <div class="env-img">
+            <label for="imagem">Alterar imagem:</label>
+            <input
+                type="file"
+                id="imagem"
+                name="imagem"
+                accept="image/png, image/jpeg, image/jpg"
+                on:change={imagemSelecionada(event)}
+            />
+
+            <!-- ALTERAR ISTO -->
+            {#if imagem_status}
+                <img
+                    src={imagem_selecionada}
+                    alt=""
+                    width="250px"
+                    height="250px"
+                    style="border-radius: 1000px;
+                    "
+                />
+            {/if}
+        </div>
+        <input type="submit" value="enviar" />
+    </form>
+</div>
+
+<style>
+    .configurações {
+        display: flex;
+        gap: 15px;
+        height: 100%;
+        width: 90%;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        background-color: white;
+        height: 90%;
+        width: 100%;
+        padding: 20px;
+        margin-block: auto;
+        border-radius: 15px;
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.808);
+
+        && input[type="text"],
+        input[type="password"],
+        input[type="email"] {
+            font-size: large;
+            text-indent: 1em;
+            height: 70px;
+            border-radius: 90px;
+        }
+
+        && input[type="submit"] {
+            font-size: large;
+            margin-top: auto;
+            text-indent: 1em;
+            height: 70px;
+            border-radius: 90px;
+        }
+    }
+
+    form > div.env-img {
+        background-color: gray;
+        padding: 5px;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+    }
+
+    form > div.env-img > label {
+        font-size: 20pt;
+    }
+
+    form > label {
+        font-size: larger;
+    }
+
+    .seus-dados {
+        border-radius: 15px;
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.808);
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        background-color: white;
+        width: 300px;
+        height: 91%;
+        margin-block: auto;
+        padding: 15px;
+        align-items: center;
+    }
+
+    .seus-dados > h3 {
+        margin: 3px;
+    }
+</style>
